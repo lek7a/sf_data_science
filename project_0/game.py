e3b5bc856827b88cfa03a0bb3d3308b0bce2e486
lek7a <lek7a@mail.ru>
2022-07-26 08:28:25 +0300

    New game

--- project_0/game.py
+++ project_0/game.py
@@ -1,17 +1,26 @@
+'''Игра компьютер угадает число меньше чем за 20 попыток'''
+
 import numpy as np
-number = np.random.randint(1, 101) # загадываем число
+
+
+    
+min = 1
+max = 101
+
+number = np.random.randint(min, max)
+#number = 1000
 count = 0
 
 while True:
-    count += 1
-    predict_number = int(input("Угадай число от 1 до 100"))
-
-    if predict_number > number:
-        print("Число должно быть меньше!")
-
-    elif predict_number < number:
-        print("Число должно быть больше!")
+    count+=1
+    mid = int((min+max) / 2)
+    
+    if mid > number:
+        max = mid
+    
+    elif mid < number:
+        min = mid
 
     else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break # конец игры, выход из цикла
+        print(f"Компьютер угадал число за {count} попыток. Это число {number}")
+        break #конец игры выход из цикла
